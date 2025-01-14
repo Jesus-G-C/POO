4c8a346f5677ac7e804ae441be0350dc89c0844d
Jesus-G-C <gsusgaspar243>
2025-01-14 11:36:43 -0600

    commit

---
+++ poo/src/main/java/edu/jesus/gaspar/actividades/actividad_1/Starter.java
@@ -0,0 +1,8 @@
+package edu.jesus.gaspar.actividades.actividad_1;
+
+public class Starter {
+public static void main(String[] args) {
+    
+} 
+
+}
---
+++ poo/src/main/java/edu/jesus/gaspar/actividades/actividad_1/ui/CLI.java
@@ -0,0 +1,5 @@
+package edu.jesus.gaspar.actividades.actividad_1.ui;
+
+public class CLI {
+
+}
