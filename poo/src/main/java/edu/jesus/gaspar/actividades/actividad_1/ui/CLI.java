28cc979ff69706480bc46022a41f5e65a881a59f
Jesus-G-C <gsusgaspar243>
2025-01-14 11:46:30 -0600

    commit

--- poo/src/main/java/edu/jesus/gaspar/actividades/actividad_1/ui/CLI.java
+++ poo/src/main/java/edu/jesus/gaspar/actividades/actividad_1/ui/CLI.java
@@ -1,5 +1,11 @@
 package edu.jesus.gaspar.actividades.actividad_1.ui;
 
 public class CLI {
-
+    
+    public static void showMenu(){
+        System.out.println("Bienvenido a la aplicacion 1");
+        System.out.println("1. Crear carro");
+        System.out.println("2. Mostrar carros");
+    }
+    
 }
