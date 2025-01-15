378adb353de36858ce660828c8cdc6cc4115cad1
Jesus-G-C <gsusgaspar243>
2025-01-15 10:25:30 -0600

    commit

---
+++ poo/src/main/java/edu/jesus/gaspar/actividades/actividad_1/models/Estudiante.java
@@ -0,0 +1,7 @@
+package edu.jesus.gaspar.actividades.actividad_1.models;
+
+public class Estudiante {
+    String nombre;
+    String apellido;
+     
+}
--- poo/src/main/java/edu/jesus/gaspar/actividades/actividad_1/ui/CLI.java
+++ poo/src/main/java/edu/jesus/gaspar/actividades/actividad_1/ui/CLI.java
@@ -1,11 +1,47 @@
 package edu.jesus.gaspar.actividades.actividad_1.ui;
 
+import java.util.Scanner;
+
 public class CLI {
     
     public static void showMenu(){
-        System.out.println("Bienvenido a la aplicacion 1");
-        System.out.println("1. Crear carro");
-        System.out.println("2. Mostrar carros");
+        System.out.println("1. Crea nuevo estudiante");
+        System.out.println("2. Listar estudiantes");
+        System.out.println("2. Salir");
     }
     
+    public void showmessage(String message){
+        System.out.println(message);
+    }
+
+    public void showerror(String message){
+        System.out.println("Error"+message);
+    }
+
+    public void showstudents(String[] students){
+        for (String student : students){
+            System.out.println(student);
+        }
+    }
+
+    public static void launch(){
+        showMenu();
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        while (option != 3){
+            switch (option){
+                case 1:
+                    System.out.println("Crear un nuevo estudiante");
+                    break;
+                case 2:
+                    System.out.println("Listar estudiantes");
+                    break;
+                case 3:
+                    System.out.println("Opcion invalida");
+                    break;
+            }
+            showMenu();
+            option = scanner.nextInt();
+        }
+    }
 }
