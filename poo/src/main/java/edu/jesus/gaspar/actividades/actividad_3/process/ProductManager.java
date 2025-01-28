2a4e594567c3fd7bde2edb5c56fe7ec931fd6740
Jesus-G-C <gsusgaspar243>
2025-01-28 12:00:49 -0600

    "Se agregan codigos para la ui, ProductManager y Product, se realiza el menu con el usuario y calculador de precios" commit

--- poo/src/main/java/edu/jesus/gaspar/actividades/actividad_3/process/ProductManager.java
+++ poo/src/main/java/edu/jesus/gaspar/actividades/actividad_3/process/ProductManager.java
@@ -1,5 +1,40 @@
 package edu.jesus.gaspar.actividades.actividad_3.process;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.jesus.gaspar.actividades.actividad_3.models.Product;
+
 public class ProductManager {
 
+     ArrayList<Product> stock;
+
+     public ProductManager(){
+        this.stock = new ArrayList<>();
+    }
+
+    public void addProduct(String name, double price, double beforeTax, double afterTax){
+        Product product = new Product(price, name, beforeTax, afterTax);
+        stock.add(product);
+    }       
+
+    public List<Product> getStock(){
+        return this.stock;
+    }
+
+    public static int cheaperProduct(Product product[]) {
+        double price;
+        int  indice = 0;
+
+        price = product[0].getPrice();
+
+        for (int i=1; i<product.length;i++){
+          if (product[i].getPrice()<price){
+            price = product[i].getPrice();
+          indice = i;
+          }
+        }
+
+        return indice;
+      }
 }
--- poo/src/main/java/edu/jesus/gaspar/actividades/actividad_3/ui/CLI.java
+++ poo/src/main/java/edu/jesus/gaspar/actividades/actividad_3/ui/CLI.java
@@ -1,5 +1,64 @@
 package edu.jesus.gaspar.actividades.actividad_3.ui;
 
+import java.util.Scanner;
+
+import edu.jesus.gaspar.actividades.actividad_3.models.Product;
+import edu.jesus.gaspar.actividades.actividad_3.process.ProductManager;
+
 public class CLI {
 
+    public static void runApp(){
+        Scanner scanner = new Scanner(System.in);
+        ProductManager productManager = new ProductManager();
+
+        showMenu();
+        int option = scanner.nextInt();scanner.nextLine();
+
+        while (option != 3){
+            switch (option){
+                case 1:
+                    System.out.println("Los modelos disponibles uno contra otro:");  
+                    formatProduct(Product product);
+                    ProductManager.cheaperProduct(Product product[]);
+                        
+                    break;
+
+                case 2:
+                System.out.print("Ingrese el nombre del producto: \n");
+                String name = scanner.nextLine();
+                System.out.print("Ingrese el precio del producto: \n");
+                double price = scanner.nextDouble();
+                double beforeTax = price + (price*.20);
+                double afterTax = beforeTax + (beforeTax*.162);
+                productManager.addProduct(name, price, beforeTax, afterTax);
+
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida");
+            }
+            showMenu();
+            option = scanner.nextInt();scanner.nextLine();
+        }
+    }
+
+    /**
+     * Imprime el texto del menu que el usuario va ver de primera mano
+     */
+    public static void showMenu(){
+        System.out.println("""
+            \n Bienvenido, ¿Que desea hacer?
+            1. Comparar modelos 
+            2. Agregar modelo 
+            3. Salir
+            """);
+    }
+
+    public static void formatProduct(Product product){
+        System.out.println("\n" +
+                    "modelo ='" + product.getName() + '\'' +
+                    ", precio ='" + product.getPrice() + '\'' +
+                    ", Antes de impuestos ='" + product.getBeforeTax() + '\'' +
+                    ", Después de impuestos ='" + product.getAfterTax() + '\'' );
+    }
 }
